9d13c9c871d34d539e417b100cabae288640f353
shephexd <shephexd@gmail.com>
2022-03-26 00:57:47 +0900

    #7 ADD backtest result API

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import io
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from api.reply.router import BotRouter
 from api.kakaoi.request import SkillPayload
-from api.firebase.realtime import key_store
+
+from api.firebase import realtime, storage
 
 
 app = FastAPI()
@@ -10,30 +13,37 @@ bot_router = BotRouter()
 
 @app.post("/api/v1/kakaoi/reply")
 def reply_kakaoi(payload: SkillPayload):
-    return bot_router.reply(payload=payload)
+    reply = bot_router.reply(payload=payload)
+    return reply.dict(exclude_none=True)
 
 
 @app.post("/api/v1/kakaoi/reply/echo")
 def echo_kakao(payload: SkillPayload):
+    print("INPUT", payload.dict(exclude_none=True))
     reply = bot_router.reply(payload=payload)
-    print(payload.dict())
-    print(reply.dict())
-    return reply.dict()
+    print("OUTPUT", reply.dict(exclude_none=True))
+    return reply.dict(exclude_none=True)
 
 
 @app.get("/api/v1/firebase/items/")
 def get_key():
-    dic = key_store.get("")
+    dic = realtime.key_store.get("")
     return list(dic.keys())
 
 
 @app.get("/api/v1/firebase/items/{item_id}")
 def get_key(item_id: str):
-    dic, _ = key_store.get(item_id)
+    dic, _ = realtime.key_store.get(item_id)
     return dic[item_id]
 
 
 @app.post("/api/v1/firebase/items")
 def update_key(item_id: str, payload: dict):
-    key_store.update({item_id: payload})
+    realtime.key_store.update({item_id: payload})
     return payload
+
+
+@app.get("/api/v1/backtest/result")
+def get_file():
+    b = io.BytesIO(storage.get_file(file_name="backtest/backtest.png"))
+    return StreamingResponse(b, media_type="image/png")
